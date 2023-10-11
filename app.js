f635e5a99f2df93ec83ff023a9ec2f92591db7ec
maximp7 <144476708+maximp7@users.noreply.github.com>
2023-10-11 13:14:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ console.log(arr[1]);
 console.log(arr[2]);
 console.log(arr[3]); 
 
-const arr2 = [true, false, "yay!", [3,6,7], null, 9];
+const arr2 = [[3,6,7], 9];
 
 arr2.unshift("pushed");
 arr2.pop();
 console.log(arr2);
+
+
+l
