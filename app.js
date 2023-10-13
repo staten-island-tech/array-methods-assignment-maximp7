9c347dd29a092a23c5eb6ff867787235b2c75000
maximp7 <144476708+maximp7@users.noreply.github.com>
2023-10-13 13:12:23 -0400

    wrk

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
- const arr = [3, 6, 9, 12];
+const Apples = {
+    Name: "Apples",
+    Group: "Fruit",
+    Healthy: "Yes",
+    Color: ["Red, Yellow, Green"],
+    Taste: "Sweet",
+    Grown: "trees",
+};
+const Strawberry = {
+    Name: "Strawberry",
+    Group: "Berry",
+    Healthy: "Yes",
+    Color: ["Red, Green"],
+    Taste: "Sweet",
+    Grown: "Vine",
+};
+const Pear = {
+    Name: "Pear",
+    Group: "Fruit",
+    Healthy: "Yes",
+    Color: "Green",
+    Taste: "Sweet",
+    Grown: "Tree",
+};
+const Grape = {
+    Name: "Grape",
+    Group: "Fruit",
+    Healthy: "Yes",
+    Color: [" Red, Purple, Green"],
+    Taste: "Sweet",
+    Grown: "Vine",
+};
+const Watermelon = {
+    Name: "Watermelon",
+    Group: "Fruit",
+    Healthy: "Yes",
+    Color: ["Red and Green"],
+    Taste: "Sweet",
+    Grown: "Vine",
+};
+let foods = [Apples, Strawberry, Pear, Grape, Watermelon];
+foods.forEach((food) => console.log(food.Name));
+foods.forEach((food) => console.log(food.Name, food.Group));
+const Healthy = foods.filter(foods => {
+    return foods.Grown.includes("Tree");
+});
+Healthy.forEach((Healthy) => console.log(Healthy.Name, Healthy.Grown));
 
-console.log(arr[0]);
-console.log(arr[1]);
-console.log(arr[2]);
-console.log(arr[3]); 
-
-const arr2 = [[3,6,7], 9];
-
-arr2.unshift("pushed");
-arr2.pop();
-console.log(arr2);
-
-
-l
