e2620bfd4ff6808d668cf53f5c91e0d41b9f27f2
maximp7 <144476708+maximp7@users.noreply.github.com>
2023-10-17 10:16:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,48 +1,55 @@
 const Apples = {
-    Name: "Apples",
-    Group: "Fruit",
-    Healthy: "Yes",
-    Color: ["Red, Yellow, Green"],
-    Taste: "Sweet",
-    Grown: "trees",
+  Name: "Apples",
+  Group: "Fruit",
+  Healthy: "Yes",
+  Color: ["Red, Yellow, Green"],
+  Taste: "Sweet",
+  Grown: "trees",
 };
 const Strawberry = {
-    Name: "Strawberry",
-    Group: "Berry",
-    Healthy: "Yes",
-    Color: ["Red, Green"],
-    Taste: "Sweet",
-    Grown: "Vine",
+  Name: "Strawberry",
+  Group: "Berry",
+  Healthy: "Yes",
+  Color: ["Red, Green"],
+  Taste: "Sweet",
+  Grown: "Vine",
 };
 const Pear = {
-    Name: "Pear",
-    Group: "Fruit",
-    Healthy: "Yes",
-    Color: "Green",
-    Taste: "Sweet",
-    Grown: "Tree",
+  Name: "Pear",
+  Group: "Fruit",
+  Healthy: "Yes",
+  Color: "Green",
+  Taste: "Sweet",
+  Grown: "Tree",
 };
 const Grape = {
-    Name: "Grape",
-    Group: "Fruit",
-    Healthy: "Yes",
-    Color: [" Red, Purple, Green"],
-    Taste: "Sweet",
-    Grown: "Vine",
+  Name: "Grape",
+  Group: "Fruit",
+  Healthy: "Yes",
+  Color: [" Red, Purple, Green"],
+  Taste: "Sweet",
+  Grown: "Vine",
 };
 const Watermelon = {
-    Name: "Watermelon",
-    Group: "Fruit",
-    Healthy: "Yes",
-    Color: ["Red and Green"],
-    Taste: "Sweet",
-    Grown: "Vine",
+  Name: "Watermelon",
+  Group: "Fruit",
+  Healthy: "Yes",
+  Color: ["Red and Green"],
+  Taste: "Sweet",
+  Grown: "Vine",
 };
 let foods = [Apples, Strawberry, Pear, Grape, Watermelon];
 foods.forEach((food) => console.log(food.Name));
-foods.forEach((food) => console.log(food.Name, food.Group));
-const Healthy = foods.filter(foods => {
-    return foods.Grown.includes("Tree");
+foods.forEach((food) => {
+    if ((food.Taste)) {
+      food.Taste.forEach((taste) => {
+        console.log(taste);
+      });
+    } else {
+      console.log(food.Taste);
+    }
+  });
+const Healthy = foods.filter((foods) => {
+  return foods.Grown.includes("Tree");
 });
 Healthy.forEach((Healthy) => console.log(Healthy.Name, Healthy.Grown));
-
